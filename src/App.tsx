83452653460d5c77514411f0d9d22f705049b177
RazTutu <raztutu123@gmail.com>
2023-01-04 14:28:13 -0800

    add basic route example with react-router-dom 6

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home"
+import AuthPage from "./pages/AuthPage";
+
+import './App.css'
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="auth" element={ <AuthPage/> } />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect} from 'react'
+import {
+  Link
+} from "react-router-dom";
+import axios from "axios";
+
+const Home = () => {
+
+  useEffect(() => {
+    // check if user exists or navigate on /login
+    // console.log(Object.keys(user).length)
+    axios.get(`http://localhost:4000/auth/login/success`, {withCredentials:true})
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.error) {
+        }
+      })
+      .catch(err => {
+        console.log("can not get current user")
+      })
+  }, [])
+
+  return (
+    <div className="homePage">
+      this is the home page
+      <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/auth">Auth page</Link>
+            </li>
+          </ul>
+    </div>
+  )
+}
+
+export default Home
