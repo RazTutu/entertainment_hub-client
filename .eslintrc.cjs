c49dd12aa70a7208bd8a1737b836d34a8130f8ef
RazTutu <rtutuianu@bitdefender.com>
2023-01-19 22:25:57 +0200

    configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  rules: {
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["@/features/*/*"],
+      },
+    ],
+    "linebreak-style": ["error", "windows"],
+    "@typescript-eslint/no-var-requires": "off",
+  },
+  overrides: [
+    {
+      files: ["*.test.{ts,tsx}", "src/testing/**/*.{ts,tsx}"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+      },
+    },
+  ],
+};
