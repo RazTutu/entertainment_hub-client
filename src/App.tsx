ddcf99af1242e683925f2fbda192672eeb4e8882
RazTutu <rtutuianu@bitdefender.com>
2023-01-19 22:35:28 +0200

    configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import Home from "./pages/Home"
-import AuthPage from "./pages/AuthPage";
+import Home from './pages/Home';
+import AuthPage from './pages/AuthPage';
 
-import './App.css'
+import './App.css';
 function App() {
-
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="auth" element={ <AuthPage/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="auth" element={<AuthPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect} from 'react'
-import {
-  Link
-} from "react-router-dom";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
-
   useEffect(() => {
     // check if user exists or navigate on /login
     // console.log(Object.keys(user).length)
-    axios.get(`http://localhost:4000/auth/login/success`, {withCredentials:true})
+    axios
+      .get(`http://localhost:4000/auth/login/success`, {
+        withCredentials: true,
+      })
       .then((response) => {
-        console.log(response.data)
+        console.log(response.data);
         if (response.data.error) {
         }
       })
-      .catch(err => {
-        console.log("can not get current user")
-      })
-  }, [])
+      .catch((err) => {
+        console.log('can not get current user');
+      });
+  }, []);
 
   const callServer = () => {
     axios({
@@ -26,34 +26,33 @@ const Home = () => {
       url: 'http://localhost:4000/games/newGame',
       data: {
         name: 'Halo Infinite',
-        platform: 'PC'
-      }
+        platform: 'PC',
+      },
     })
-    .then((response) => {
-      console.log(response.data)
-      if (response.data.error) {
-      }
-    })
-    .catch(err => {
-      console.log("can not get current user")
-    })
-  }
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.error) {
+        }
+      })
+      .catch((err) => {
+        console.log('can not get current user');
+      });
+  };
 
   return (
     <div className="homePage">
       this is the home page
       <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/auth">Auth page</Link>
-            </li>
-        
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/auth">Auth page</Link>
+        </li>
       </ul>
       <h1 onClick={callServer}>click here to call</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
