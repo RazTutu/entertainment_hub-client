038fa639198a4af0f55dde69dd889633626239a4
RazTutu <raztutu123@gmail.com>
2023-07-30 17:11:20 +0300

    feat: add storybook support

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,22 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:storybook/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-  ],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:storybook/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:storybook/recommended"],
   rules: {
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["@/features/*/*"],
-      },
-    ],
+    "no-restricted-imports": ["error", {
+      patterns: ["@/features/*/*"]
+    }],
     "linebreak-style": ["error", "windows"],
-    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-var-requires": "off"
   },
-  overrides: [
-    {
-      files: ["*.test.{ts,tsx}", "src/testing/**/*.{ts,tsx}"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-      },
-    },
-  ],
+  overrides: [{
+    files: ["*.test.{ts,tsx}", "src/testing/**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off"
+    }
+  }]
 };
